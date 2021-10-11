c21bd5a9625aed5fb389e283bda39f896e63b973
williamfzc <178894043@qq.com>
2021-10-11 11:41:27 +0000

    chore: script for debug

--- scripts/assembleAll.sh
+++ scripts/assembleAll.sh
@@ -1,10 +1,18 @@
 set -e
 set -x
 
+echo "compile callreco libs ..."
 ./gradlew clean assemble
+echo "libs compiled"
 
-rm -rf demo/android/app/libs/*
-cp callreco-android-plugin/build/libs/* demo/android/app/libs
-cp callreco-core/build/libs/* demo/android/app/libs
+ANDROID_DEMO_LIB_DIR=demo/android/app/libs
+rm -rf ${ANDROID_DEMO_LIB_DIR}
+mkdir ${ANDROID_DEMO_LIB_DIR}
+cp callreco-android-plugin/build/libs/* ${ANDROID_DEMO_LIB_DIR}
+cp callreco-core/build/libs/* ${ANDROID_DEMO_LIB_DIR}
+cp callreco-agent/build/libs/* ${ANDROID_DEMO_LIB_DIR}
+cp callreco-commons/build/libs/* ${ANDROID_DEMO_LIB_DIR}
 
-#./demo/android/gradlew clean assemble --stacktrace
+echo "compile android demo ..."
+./demo/android/gradlew clean assemble --stacktrace
+echo "android demo compiled"
