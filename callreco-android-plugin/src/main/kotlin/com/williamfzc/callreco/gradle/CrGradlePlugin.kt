c60c2f0ce612cd51cb7fb3bec3e7af17d5635d5a
williamfzc <178894043@qq.com>
2021-10-11 10:26:28 +0000

    chore(android): export method mapping after build

--- callreco-android-plugin/src/main/kotlin/com/williamfzc/callreco/gradle/CrGradlePlugin.kt
+++ callreco-android-plugin/src/main/kotlin/com/williamfzc/callreco/gradle/CrGradlePlugin.kt
@@ -1,14 +1,31 @@
 package com.williamfzc.callreco.gradle
 
 import com.android.build.gradle.TestedExtension
+import com.williamfzc.callreco.core.instr.CrWeaveRecorder
+import com.williamfzc.callreco.core.log.logE
+import com.williamfzc.callreco.core.log.logI
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.io.File
 
 class CrGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.extensions.findByType(TestedExtension::class.java)?.let { ext ->
             println("found ext in ${target.project.path}")
             ext.registerTransform(CrTransformer(target))
+        }
+
+        // export method mapping
+        target.gradle.buildFinished {
+            if (null != it.failure) {
+                logE("build failed so callreco skipped")
+                return@buildFinished
+            }
+
+            val outputTextFile = File(target.buildDir, "callreco-mapping.json")
+            outputTextFile.createNewFile()
+            outputTextFile.writeText(CrWeaveRecorder.dump())
+            logI("write method mapping to $outputTextFile")
         }
     }
 }
