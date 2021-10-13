42e61cf1aed3e4ba8eb96d610a308d1992644baa
williamfzc <178894043@qq.com>
2021-10-13 10:19:41 +0000

    chore: some cases for perf eval

--- callreco-core/src/test/kotlin/Sample.java
+++ callreco-core/src/test/kotlin/Sample.java
@@ -2,8 +2,11 @@ public class Sample {
     public Sample() {
     }
 
+    private void funcC() {
+
+    }
+
     private void funcA() {
-        System.out.println("ojbk");
         int[] a = new int[10];
         a[0]++;
         a[9]++;
@@ -11,5 +14,25 @@ public class Sample {
 
     public void funcB() {
         funcA();
+        for (int i = 0; i < 5000; i++) {
+            funcC();
+        }
+        funcE();
+    }
+
+    private void funcD() {
+
+    }
+
+    private void funcE() {
+
+    }
+
+    private void funcF() {
+
+    }
+
+    private void funcG() {
+
     }
 }
--- callreco-core/src/test/kotlin/TestSmoke.kt
+++ callreco-core/src/test/kotlin/TestSmoke.kt
@@ -2,33 +2,41 @@ import com.williamfzc.callreco.agent.rt.CrRuntime
 import com.williamfzc.callreco.core.analyze.CrAnalyzer
 import com.williamfzc.callreco.core.instr.CrInstrumentor
 import com.williamfzc.callreco.core.instr.CrWeaveRecorder
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.lang.reflect.Method
 import java.net.URLClassLoader
 import java.util.concurrent.TimeUnit
 
 
 class TestSmoke {
-    @Test
-    fun testInst() {
+    private val prefix = "src/test/kotlin"
+    private val srcFile = "${prefix}/Sample.java"
+    private val clzFile = "${prefix}/Sample.class"
+    private val beforeClazzFile = File(clzFile)
+    private val afterClazzFile = File(clzFile)
+
+    @BeforeEach
+    fun beforeTest() {
         // compile
-        val prefix = "src/test/kotlin"
-        val srcFile = "${prefix}/Sample.java"
-        val clzFile = "${prefix}/Sample.class"
         ProcessBuilder("javac $srcFile".split(" "))
                 .start()
                 .waitFor(15, TimeUnit.SECONDS)
 
         // weave
-        val beforeFile = File(clzFile)
-        val afterByte = CrInstrumentor.instrument(beforeFile.readBytes())
-        val afterClassFile = File(clzFile).apply {
+        val afterByte = CrInstrumentor.instrument(beforeClazzFile.readBytes())
+        afterClazzFile.apply {
             createNewFile()
             writeBytes(afterByte)
         }
+    }
 
+    @Test
+    fun testInst() {
         // reload weaved class
-        val afterClassUrl = afterClassFile.parentFile.toURI().toURL()
+        val afterClassUrl = afterClazzFile.parentFile.toURI().toURL()
         val cl: ClassLoader = URLClassLoader(arrayOf(afterClassUrl))
         val sampleClazz = cl.loadClass("Sample")
         sampleClazz.declaredMethods.forEach { each ->
@@ -57,5 +65,44 @@ class TestSmoke {
                 println("${eachMethod.methodInfo}, hit: ${eachMethod.hitCount}")
             }
         }
+    }
+
+    @Test
+    fun testPerf() {
+        // reload weaved class
+        val afterClassUrl = afterClazzFile.parentFile.toURI().toURL()
+        val cl: ClassLoader = URLClassLoader(arrayOf(afterClassUrl))
+        val afterSampleClazz = cl.loadClass("Sample")
+        afterSampleClazz.declaredMethods.forEach { each ->
+            println(each.toGenericString())
+        }
+
+        // load origin class
+        val beforeClassUrl = beforeClazzFile.parentFile.toURI().toURL()
+        val oldCl: ClassLoader = URLClassLoader(arrayOf(beforeClassUrl))
+        val beforeSampleClazz = oldCl.loadClass("Sample")
+
+        //
+        val afterInst = afterSampleClazz.newInstance()
+        val afterFuncB = afterSampleClazz.getDeclaredMethod("funcB")
+        val beforeInst = beforeSampleClazz.newInstance()
+        val beforeFuncB = beforeSampleClazz.getDeclaredMethod("funcB")
+
+        // test
+        val afterCost = pressureCall(afterFuncB, afterInst)
+        val beforeCost = pressureCall(beforeFuncB, beforeInst)
+        println("cost: $beforeCost -> $afterCost")
+        // check CrRuntime
+        val rtData = CrRuntime.INSTANCE.dump()
+        println("ret: $rtData")
+    }
+
+    private fun pressureCall(m: Method, i: Any): Double {
+        val start = System.currentTimeMillis()
+        val count = 100000000
+        for (ignored in 0..count) {
+            m.invoke(i)
+        }
+        return (System.currentTimeMillis() - start) / (count * 1.0)
     }
 }
